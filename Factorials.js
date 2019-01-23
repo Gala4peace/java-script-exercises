a719d04f9b673dded798488cd990ab18b667141d
Galatea Mancini <gala@galateas-mbp-2.ginetta.local>
2019-01-23 09:20:38 +0100

    Add: the latest exercises on .js  functions and loops

---
+++ Factorials.js
@@ -0,0 +1,7 @@
+var num = 12;
+undefined
+var x = 1;
+undefined
+for (var num =12; num >= 1; --num;) {
+    num * num -1}    ;
+    
---
+++ index.js
@@ -0,0 +1,3 @@
+for (var i= 0; 0 < 11; i = i+2){ 
+  console.log("this will print all increments of 2=" + i);
+  }
