197a60d29752e0e1a0268095b612e2edf069165c
Galatea Mancini <gala@Galateas-MBP-2.fritz.box>
2019-01-23 21:08:23 +0100

    Add Functions and arreys exercises

---
+++ cry.js
@@ -0,0 +1,7 @@
+var cry = function(num){
+  var string = "";
+  string +=  "boohoo" ;
+}
+return string +"!";
+}
+console.log(cry);
--- laugh-it-off.js
+++ laugh-it-off.js
@@ -1,9 +1,17 @@
 /*
  * Programming Quiz: Laugh it Off 2 (5-2)
  *
- * Write a function called `laugh` with a parameter named `num` that represents the number of "ha"s to return.
+ * Write a function called `laugh` with a parameter named `num` that represents
+ the number of "ha"s to return.
  *
  * Note:
  *  - make sure your the final character is an exclamation mark ("!")
- *  - make sure that your function produces the correct results when it is called multiple times
+ *  - make sure that your function produces the correct results when it is
+      called multiple times
  */
+ function laugh(num) {
+
+   for(var num= "ha"; num < 3; num = num + 1);
+ }
+ laugh(num);
+ console.log(laugh(3) + "!");
